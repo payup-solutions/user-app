7374d3cf5c27b65dd4289fea6f186606951d3319
Vinicius Melo <vinicius.payup@gmail.com>
2017-09-21 22:48:23 -0300

    Page codes

--- src/pages/codes/codes.ts
+++ src/pages/codes/codes.ts
@@ -9,14 +9,7 @@ import 'rxjs/add/operator/map';
 })
 export class CodesPage {
   public feeds: Array<string>;
-  private url: string = "https://www.reddit.com/new.json";  
+  private url: string = "https://www.reddit.com/new.json";
 
-  //constructor(public navCtrl: NavController) {}
-
-  constructor(public navCtrl: NavController, public http: Http) {
-    this.http.get(this.url).map(res => res.json())
-    .subscribe(data => {
-      this.feeds = data.data.children;
-    }); 
-  }
+  constructor(public navCtrl: NavController) { }
 }
